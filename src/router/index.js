b397ae4d678505d4e9328fa8e8d56081f6697ae5
Nicholas Gambino <nicholasgambino@Nicholass-MBP.hawaii.rr.com>
2019-05-31 15:35:18 -1000

    Refactor react-router to reach-router for accesibility
    Reach router seems to be more syntactically logical, and built to be more accesible.

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 
 import Home from "../components/home";
 import Login from "../components/login";
 
-const ReactRouter = () => {
-  return (
+const ReactRouter = () => (
+  <div>
+    <ul>
+      <li>
+        <Link to="/">Login</Link>
+      </li>
+      <li>
+        <Link to="home">Home</Link>
+      </li>
+    </ul>
+    <hr />
     <Router>
-      <div>
-        <Link to="/">Return to login</Link>
-        <hr />
-      </div>
-      <Route exact path="/" component={Login} />
-      <Route path="/home" component={Home} />
+      <Login path="/" />
+      <Home path="/home" />
     </Router>
-  );
-};
+  </div>
+);
 
 export default ReactRouter;
