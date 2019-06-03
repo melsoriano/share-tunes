ef4f32036b5851ca1e98b07cdfb4158042028588
jhoun <jayhoun@gmail.com>
2019-06-03 09:04:08 -1000

    Update with react reach router

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -13,9 +13,9 @@ function Login() {
       });
   }
 
-  useEffect(() => {
-    fetchMyAPI();
-  }, []);
+  // useEffect(() => {
+  //   fetchMyAPI();
+  // }, []);
 
   return (
     <div className="App">
